84d28700af15490e3b85053a92a75a33e89298f5
Dickinson, Joseph (Associated) <joseph.dickinson@spglobal.com>
2019-10-25 15:07:38 -0600

    added ang material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { DataComponent } from './data/data.component';
 import { DriverComponent } from './driver/driver.component';
 import { SpcontainerComponent } from './spcontainer/spcontainer.component';
 import { SprowviewComponent } from './sprowview/sprowview.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -18,7 +19,8 @@ import { SprowviewComponent } from './sprowview/sprowview.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
